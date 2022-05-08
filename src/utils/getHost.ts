b7eb4e4f7670fbeb3e829eadccc7dbcfa0f43d39
osa9 <osanine9@gmail.com>
2022-05-08 20:03:34 +0900

    get host behind CDN

--- src/utils/getHost.ts
+++ src/utils/getHost.ts
@@ -4,23 +4,28 @@
 // This is not production ready, (except with providers that ensure a secure host, like Now)
 // For production consider the usage of environment variables and NODE_ENV
 function getHost(req) {
-    if (!req) return ''
+  if (!req) return ''
 
-    let host;
-    if (req.headers) {
-        // server side
-        host = req.headers.host;
-    } else if (req.location) {
-        // client side
-        host = req.location.host;
-    } else {
-        throw Error('getHost: Unknown parameter');
-    }
+  let host
+  if (req.headers) {
+    // server side
+    if (req.headers['x-forwarded-host']) host = req.headers['x-forwarded-host']
+    else host = req.headers.host
+  } else if (req.location) {
+    // client side
+    host = req.location.host
+  } else {
+    throw Error('getHost: Unknown parameter')
+  }
 
-    if (host.startsWith('localhost') || host.startsWith('10.') || host.startsWith('192.')) {
-        return `http://${host}`
-    }
-    return `https://${host}`
+  if (
+    host.startsWith('localhost') ||
+    host.startsWith('10.') ||
+    host.startsWith('192.')
+  ) {
+    return `http://${host}`
+  }
+  return `https://${host}`
 }
 
-export default getHost;
+export default getHost
